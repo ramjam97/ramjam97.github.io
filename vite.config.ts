851b8b7a233ecbca745bec289519fb36c40d2cc2
Ramjam97 <ram.jamolod@gmail.com>
2025-10-25 14:30:49 +0800

    remove unnecessary packages

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { fileURLToPath } from 'url'
-import dotenv from 'dotenv'
 import { dirname, resolve } from 'path'
 
 // Get __dirname equivalent in ESM
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
-
-dotenv.config()
 
 // https://vite.dev/config/
 export default defineConfig({
