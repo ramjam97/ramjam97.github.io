8f18545f5529429d881e6d9546f4feb0f4e7ae2c
Ramjam97 <ram.jamolod@gmail.com>
2025-11-28 16:58:17 +0800

    added shuffle for default theme

---
+++ src/utils/universal.tsx
@@ -0,0 +1,8 @@
+export function array_shuffle(array: string[] = []) {
+    if (!Array.isArray(array)) return [];
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]]; // swap
+    }
+    return array;
+}
