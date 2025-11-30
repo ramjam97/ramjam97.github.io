b4c93a23958d51330a18580d9521c870cf36145a
Ramjam97 <ram.jamolod@gmail.com>
2025-11-30 14:32:07 +0800

    fixing bg color of certificate item indicator

--- src/components/CertificateItem.tsx
+++ src/components/CertificateItem.tsx
@@ -7,7 +7,7 @@ interface CertItemProps {
 export default function CertificateItem({ item }: CertItemProps) {
     return <>
         <span className="relative">
-            <span className="bg-white border border-success absolute left-0 w-2 h-2 absolute top-3 left-0 transform translate-y-[-50%] translate-x-[-250%] rounded-full"></span>
+            <span className="bg-base-200 border border-success absolute left-0 w-2 h-2 absolute top-3 left-0 transform translate-y-[-50%] translate-x-[-250%] rounded-full"></span>
             <a href={item.url} target="_blank">
                 <div className="font-bold link-hover">{item.title}</div>
             </a>
