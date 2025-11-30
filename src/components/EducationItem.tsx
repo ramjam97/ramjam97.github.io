a2430ddf2914722a1194c2fa98a4eb543f1cf183
Ramjam97 <ram.jamolod@gmail.com>
2025-11-30 14:40:04 +0800

    minor fixes

--- src/components/EducationItem.tsx
+++ src/components/EducationItem.tsx
@@ -19,9 +19,9 @@ export default function EducationItem({ index, total, item }: EducItemProps) {
     return <>
         <div className={`border-secondary border-s pt-7 pb-3 ps-2 relative`}>
 
-            <span className={`px-2 pe-4 absolute top-0 left-0 bg-secondary text-secondary-content`}>
+            <span className="px-2 pe-4 absolute top-0 left-0 bg-secondary text-secondary-content">
                 <span>{displayDateRange} · {dateDiff.text}</span>
-                <span className={`bg-secondary border border-secondary absolute top-[50%] left-0 transform translate-y-[-50%] translate-x-[-50%] rotate-45 w-2 h-2`}></span>
+                <span className="bg-secondary border border-secondary absolute top-[50%] left-0 transform translate-y-[-50%] translate-x-[-50%] rotate-45 w-2 h-2"></span>
                 <span className="absolute top-[50%] right-0 w-4 h-4 bg-base-200 transform translate-y-[-40%] translate-x-[50%] rotate-45"></span>
             </span>
 
--- src/components/ExperienceItem.tsx
+++ src/components/ExperienceItem.tsx
@@ -30,9 +30,9 @@ export default function ExperienceItem(props: ExperiencesItemProps) {
     return <>
         <div className="border-info border-s pt-7 pb-3 ps-2 relative">
 
-            <span className={`px-2 pe-4 absolute top-0 left-0 bg-info text-info-content`}>
+            <span className="px-2 pe-4 absolute top-0 left-0 bg-info text-info-content">
                 <span>{displayDateRange} · {dateDiff.text}</span>
-                <span className={`bg-info border border-info absolute top-[50%] left-0 transform translate-y-[-50%] translate-x-[-50%] rotate-45 w-2 h-2`}></span>
+                <span className="bg-info border border-info absolute top-[50%] left-0 transform translate-y-[-50%] translate-x-[-50%] rotate-45 w-2 h-2"></span>
                 <span className="absolute top-[50%] right-0 w-4 h-4 bg-base-200 transform translate-y-[-40%] translate-x-[50%] rotate-45"></span>
             </span>
 
