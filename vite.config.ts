ec99e9ba29a0dcc2b34ccacf38630eeab384527e
Ramjam97 <ram.jamolod@gmail.com>
2025-12-06 21:22:30 +0800

    fixing vite error

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { fileURLToPath } from 'url'
 import { dirname, resolve } from 'path'
-import { meta } from './src/constant/metaConfig';
 
 // Get __dirname equivalent in ESM
 const __filename = fileURLToPath(import.meta.url)
@@ -13,9 +12,6 @@ const __dirname = dirname(__filename)
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-    },
-  },
-  define: { meta }
+    alias: { '@': resolve(__dirname, './src') }
+  }
 })
