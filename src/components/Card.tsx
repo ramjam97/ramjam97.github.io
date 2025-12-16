09c319bcf62d960890da5ffbd54aa6bca7ec2ccc
Ramjam97 <ram.jamolod@gmail.com>
2025-12-16 22:27:55 +0800

    modify shadow

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,7 +6,7 @@ interface CardProps {
 
 export default function Card({ id, title, children }: CardProps) {
     return <>
-        <div className="card bg-base-200 w-full shadow shadow-lg border border-base-300" id={id}>
+        <div className="card bg-base-200 w-full shadow shadow-md border border-base-300" id={id}>
             <div className="card-body p-3">
                 {title && <h2 className="card-title text-primary text-xl py-1">{title}</h2>}
                 {children}
